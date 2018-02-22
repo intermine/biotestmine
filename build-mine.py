59077a3cafd8127f2f006910d8959a635cd0a89b
Justin Clark-Casey <justincc@justincc.org>
2018-02-22 13:35:44 +0000

    add a --dry-run option

--- build-mine.py
+++ build-mine.py
@@ -1,29 +1,44 @@
 #!/usr/bin/python3
 
+import argparse
 import subprocess
 
 
-def run(cmd):
+# FUNCTIONS
+def run(cmd, _options):
     print('Running:', ' '.join(cmd))
+
+    if _options['dry-run']:
+        return
+
     rc = subprocess.call(cmd)
     if rc != 0:
         print('Command [%s] failed with rc %d' % (' '.join(cmd), rc))
         exit(1)
 
 
-def integrate_source(source_name):
-    run(['./gradlew', 'integrate', '-Psource=%s' % source_name, '--no-daemon'])
+def integrate_source(source_name, _options):
+    run(['./gradlew', 'integrate', '-Psource=%s' % source_name, '--no-daemon'], _options)
 
+
+# MAIN
+parser = argparse.ArgumentParser('Build the mine')
+parser.add_argument(
+    '--dry-run', action='store_true', default=False,
+    help='Don''t actually build anything, just show the commands that would be executed')
+args = parser.parse_args()
+
+options = {'dry-run': args.dry_run}
 
 sources = ['uniprot-malaria', 'malaria-gff', 'malaria-chromosome-fasta', 'go', 'go-annotation', 'update-publications', 'entrez-organism']
 
-run(['./gradlew', 'buildDB'])
-run(['./gradlew', 'buildUserDB'])
-run(['./gradlew', 'loadDefaultTemplates'])
+run(['./gradlew', 'buildDB'], options)
+run(['./gradlew', 'buildUserDB'], options)
+run(['./gradlew', 'loadDefaultTemplates'], options)
 
 for source in sources:
-    integrate_source(source)
+    integrate_source(source, options)
 
-run(['./gradlew', 'postprocess', '--no-daemon'])
+run(['./gradlew', 'postprocess', '--no-daemon'], options)
 
 print('Finished. Now run "./gradlew tomcatStartWar"')
