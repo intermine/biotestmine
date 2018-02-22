419269567f639501e7709b0c37e9193fa7b3a633
Justin Clark-Casey <justincc@justincc.org>
2018-02-22 15:54:47 +0000

    load mine properties file

--- build-mine.py
+++ build-mine.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
 
 import argparse
+import os.path
 import subprocess
+
+import jprops
 
 import interminepy.project as imp
 
 
 # FUNCTIONS
+def check_path_exists(path):
+    if not os.path.exists(path):
+        print('Could not find %s. Exiting' % path)
+        exit(1)
+
+
 def run(cmd, _options):
     print('Running:', ' '.join(cmd))
 
@@ -26,11 +35,22 @@ def integrate_source(_source, _options):
 # MAIN
 parser = argparse.ArgumentParser('Build the mine')
 parser.add_argument(
+    'mine_properties_path', help="path to the mine's properties file, e.g. ~/.intermine/biotestmine.properties")
+parser.add_argument(
     '--dry-run', action='store_true', default=False,
     help='Don''t actually build anything, just show the commands that would be executed')
 args = parser.parse_args()
 
+if not os.path.exists(args.mine_properties_path):
+    print('Could not find %s. Exiting' % args.mine_properties_path)
+    exit(1)
+
+check_path_exists(args.mine_properties_path)
+check_path_exists('project.xml')
 options = {'dry-run': args.dry_run}
+
+with open(args.mine_properties_path) as f:
+    props = jprops.load_properties(f)
 
 with open('project.xml') as f:
     project = imp.Project(f)
